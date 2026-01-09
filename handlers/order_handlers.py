726565459c71a1203f257b590beaef2643555398
Sergey <65830968+PS-Bot-Maker@users.noreply.github.com>
2026-01-09 13:50:55 +0300

    Update order_handlers.py

--- handlers/order_handlers.py
+++ handlers/order_handlers.py
@@ -1,9 +1,9 @@
-from aiogram import Router, types, F
-from aiogram.filters.command import Command
-from aiogram.types import FSInputFile 
-import os
+#from aiogram import Router, types, F
+#from aiogram.filters.command import Command
+#from aiogram.types import FSInputFile 
+#import os
 
 
-order_han_router = Router()
+#order_han_router = Router()
 
 #@main_han_router
