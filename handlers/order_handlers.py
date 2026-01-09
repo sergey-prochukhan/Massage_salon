b279f559261a42fb60357ae28aebc91d10dda170
PS-Bot-Maker <sergeyprochuhan@gmail.com>
2026-01-09 17:05:15 +0300

    comment bag

--- handlers/order_handlers.py
+++ handlers/order_handlers.py
@@ -1,15 +1,15 @@
 from aiogram import Router, types, F
 from aiogram.filters.command import Command
-from main import bot
+#from main import bot
 
 
 order_han_router = Router()
 
-@order_han_router.message(Command("ordering"))
-async def show_alert_ordering(callback_query: types.CallbackQuery):
-    await bot.answer.callback_query(callback_query.id,
-    text="Всё получилось", 
-    show_alert=True
-    )
+#@order_han_router.message(Command("ordering"))
+#async def show_alert_ordering(callback_query: types.CallbackQuery):
+ #   await bot.answer.callback_query(callback_query.id,
+  #  text="Всё получилось", 
+   # show_alert=True
+    #)
 
 #await callback.answer()
