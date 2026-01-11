a5c5da4336b0bbdbc503f2045208469e0cde5e8c
sergey-prochukhan <sergeyprochuhan@gmail.com>
2026-01-11 12:43:21 +0300

    Add 'approve' functional (geting user_id and user_name), prepare to add sqlite3

--- handlers/menu_handlers.py
+++ handlers/menu_handlers.py
@@ -180,10 +180,8 @@ async def callback_handler(callback: types.CallbackQuery):
         except Exception as e:
             await callback.answer(f"Ошибка: {e}")
 
-    #Обрабатываем кнопку "Согласен(а)"
+
     
-    elif callback.data == "approve":
-        await callback.answer(f"Функционал в разработке\n Будет доступен со следующим обновлением.", show_alert=True)
 
     #Обрабатываем кнопку "Скачать"
     elif callback.data == "sendmedoc":
--- handlers/order_handlers.py
+++ handlers/order_handlers.py
@@ -9,10 +9,13 @@ order_han_router = Router()
 #
 # Определяем состояния
 class OrderStates(StatesGroup):
+    wait_user_name = State()
     wait_massage_type = State()
     wait_date = State()
     wait_contact = State()
 
+
+#Обрабатываем кнопку "Согласен(а)"
 @order_han_router.callback_query(F.data == "approve")
 async def start_ordering(callback: CallbackQuery, state: FSMContext):
     try:
@@ -24,12 +27,21 @@ async def start_ordering(callback: CallbackQuery, state: FSMContext):
 
         await callback.bot.send_message(
             chat_id=callback.message.chat.id,
-            text="Какой вид массажа вас интересует?"
+            text="Как к вам обращаться?"
         )
-        await state.set_state(OrderStates.wait_massage_type)  # Переходим в состояние
+        await state.set_state(OrderStates.wait_user_name)  # Переходим в состояние
     except Exception as e:
         await callback.answer(f"Ошибка: {e}")
     await callback.answer()
+
+@order_han_router.message(OrderStates.wait_user_name)
+async def get_massage_type(message: types.Message, state: FSMContext):
+    user_t_id = message.from_user.id
+    user_name = message.text  # Сохраняем ответ
+    await state.update_data(name=user_name, user_id=user_t_id)  # Записываем в FSM
+
+    await message.answer("Какой вид массажа вас интересует?")
+    await state.set_state(OrderStates.wait_massage_type)
 
 @order_han_router.message(OrderStates.wait_massage_type)
 async def get_massage_type(message: types.Message, state: FSMContext):
@@ -54,16 +66,19 @@ async def get_contact(message: types.Message, state: FSMContext):
 
     # Получаем все сохранённые данные
     data = await state.get_data()
+    user_name = data.get("name")
+    user_t_id = data.get("user_id")
     user_m_type = data.get("massage_type")
     user_date = data.get("date")
     user_cont_inf = data.get("contact")
 
     await message.answer(
-        f"Спасибо за заявку!\n"
+        f"{user_name} Спасибо за заявку!\n"
         f"Вид массажа: {user_m_type}\n"
         f"Дата: {user_date}\n"
         f"Контакт: {user_cont_inf}\n"
-        f"Мы скоро с вами свяжемся.\n"
+        f"Ваш ID{user_t_id}\n"
+        f"Подтвердите ваш заказ и мы скоро с вами свяжемся.\n"
         f"Чтобы вернуться в меню, нажмите /menu"
     )
 
